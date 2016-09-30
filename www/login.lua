beb90e0e3e317faef0d6ffdc63edbcdbb287e6fe
columna1 <thecolumna@gmail.com>
2016-09-30 08:37:44 -0400

    nope

--- www/login.lua
+++ www/login.lua
@@ -9,7 +9,7 @@ else
 	--http.request({url = "https://url", method = "POST"}, body)
 
 	data = https.post("https://api.twitch.tv/kraken/oauth2/token",
-	 "client_id=dzyhrtippxss3fpi4o5h6wcgauez20y"
+	 "client_id=dzyhrtippxss3fpi4o5h6wcgauez20y"--no longer valid
 	.."&client_secret=pt9h7evps1rose9okpgsszowyamm8ea"
 	.."&grant_type=authorization_code"
 	.."&redirect_uri=http://localhost:8080/login.lua"
